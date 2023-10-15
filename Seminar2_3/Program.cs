4e8c28f7d891bb91944bf661becba597bfbdf138
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-15 13:10:02 +0500

    add Seminar2_3

---
+++ Seminar2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Number1- ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number2- ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1 % num2;
+if(result == 0)
+{
+    Console.Write("Первое число кратно второму");
+}
+else
+{
+     Console.Write("Первое число некратно второму, остаток" + result);
+}

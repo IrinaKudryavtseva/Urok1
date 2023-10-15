71b46d404172d2d0df4f090fbd529d82f7a08c37
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-15 12:54:45 +0500

    add Seminar2_2

---
+++ Seminar2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число ");
+string number1_str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1_str);
+Console.WriteLine("Введите второе число ");
+string number2_str = Console.ReadLine();
+int number2 = Convert.ToInt32(number2_str);
+int kvadrat1 = number2 * number2;
+int kvadrat2 = number1 * number1;
+if(kvadrat2 == number1 || kvadrat1 == number2)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}

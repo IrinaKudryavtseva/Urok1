3aa4ac7a547245997365674dc5134c2e9b8790a8
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-15 13:16:56 +0500

    add Seminar2_4

---
+++ Seminar2_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Number - ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 7 == 0 && number % 23 == 0)
+{
+   Console.Write("Yes");
+}
+else
+   Console.Write("No");
+
+

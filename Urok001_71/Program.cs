530701502d8be009412600804d50134a1fbb622e
Irina Kudriavtseva <st_irina_igorevna@mail.ru>
2023-10-15 12:40:49 +0500

    add Urok001_71

---
+++ Urok001_71/Program.cs
@@ -0,0 +1,9 @@
+﻿Random generateNumber = new Random();
+int number = generateNumber.Next(100, 1000);
+int numberA = number / 100;
+Console.WriteLine(numberA);
+int numberB = number % 10;
+Console.WriteLine(numberB);
+// Console.WriteLine(numberA.ToString() + numberB.ToString());
+// Console.WriteLine($"{numberA}" + $"{numberB}");
+Console.WriteLine(numberA * 10 + numberB);
